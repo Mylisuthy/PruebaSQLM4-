4c64605d9289c634ecaccfb2b4a61494fec8e182
Mylisuthy <juanch052048@gmail.com>
2025-08-13 08:38:49 -0500

    supabase_client.js

--- Proyect/proyect-root/backend/supabase_client.js
+++ Proyect/proyect-root/backend/supabase_client.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
-// Variables de entorno para seguridad
+// environment variables
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE || process.env.SUPABASE_SERVICE_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 module.exports = supabase;
+
